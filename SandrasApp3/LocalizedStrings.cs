e2d1cd3d6e3906a3b632c82dee413f7a5282fdda
Sandra Erbel <dont-stop-continue@gmx.net>
2015-12-16 17:39:06 +0100

    first commit

---
+++ SandrasApp3/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SandrasApp3.Resources;
+
+namespace SandrasApp3
+{
+    /// <summary>
+    /// Bietet Zugriff auf Zeichenfolgenressourcen.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ SandrasApp3/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using SandrasApp3.Resources;
+
+namespace SandrasApp3
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Konstruktor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Beispielcode zur Lokalisierung der ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void einpacken_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            MessageBox.Show("Sie haben erfolgreich eingepackt");
+        }
+
+        private void bla_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sie haben erfolgreich auf bla geklickt");
+        }
+
+        private void shareevent_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sie haben erfolgreich  auf share-event geklickt");
+        }
+
+        // Beispielcode zur Erstellung einer lokalisierten ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // ApplicationBar der Seite einer neuen Instanz von ApplicationBar zuweisen
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Eine neue Schaltfläche erstellen und als Text die lokalisierte Zeichenfolge aus AppResources zuweisen.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Ein neues Menüelement mit der lokalisierten Zeichenfolge aus AppResources erstellen
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
